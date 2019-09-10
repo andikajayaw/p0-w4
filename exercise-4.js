2ba9f93469f789db1c1171ffaa72b6e67e9c8686
andikajayaw <andikajayawiguna@gmail.com>
2019-09-10 23:26:59 +0700

    add exercise-4.js

---
+++ exercise-4.js
@@ -0,0 +1,37 @@
+/** 
+ * ALGORITMA
+ * 
+ * 1. Modus adalah angka dari sebuah deret yang paling banyak atau paling sering muncul.
+ * 2. Tampilkan bilangan yang paling sering tampil dalam satu deret bilangan
+ * 3. Jika dalam deret bilangan menampilkan angka yang sama maka itu sama dengan -1
+ * 4. Jika modus tidak ditemukan makan akan me return - 1
+ * 
+ */
+
+function cariModus(arr) {
+    // you can only write your code here!
+    var list = []
+
+    for (var i = 0; i < arr.length; i++) {
+        var isFind = false
+        for (var j = 0; j < list.length; j++) {
+            if (arr[i] === list[j][0]) {
+                isFind = true
+                list[j][1]++
+            }
+        }
+        if (isFind === false) {
+            list.push([arr[i], 1])
+        }
+    }
+
+
+    return list
+}
+
+// TEST CASES
+console.log(cariModus([10, 4, 5, 2, 4])); // 4
+// console.log(cariModus([5, 10, 10, 6, 5])); // 5
+// console.log(cariModus([10, 3, 1, 2, 5])); // -1
+// console.log(cariModus([1, 2, 3, 3, 4, 5])); // 3
+// console.log(cariModus([7, 7, 7, 7, 7])); // -1
