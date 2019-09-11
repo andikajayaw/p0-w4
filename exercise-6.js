325bcc889d8493241a2fe26de745612eed9ae0b0
andikajayaw <andikajayawiguna@gmail.com>
2019-09-11 18:41:23 +0700

    update exercise-6.js

--- exercise-6.js
+++ exercise-6.js
@@ -8,21 +8,29 @@
 
 function digitPerkalianMinimum(angka) {
     // you can only write your code here!
-    var result = 0
-    var bagi = 0
+    var temp = []
 
-    for (var i = 0; i <= angka; i++) {
-        if (angka % i == 0) {
-            result = i
-            console.log(i)
+    for (var i = 1; i <= angka; i++) {
+        if (angka % i === 0) {
+            temp.push([i, angka / i])
+        }
+    }
+
+    var result = 0
+    for (var i = 0; i < temp.length; i++) {
+        var digit = String(temp[i][0]).length + String(temp[i][1]).length
+        if (result === 0) {
+            result = digit
+        } else if (digit < result) {
+            result = digit
         }
     }
     return result
 }
 
 // TEST CASES
-console.log(digitPerkalianMinimum(24)); // 2
+// console.log(digitPerkalianMinimum(24)); // 2
 // console.log(digitPerkalianMinimum(90)); // 3
 // console.log(digitPerkalianMinimum(20)); // 2
 // console.log(digitPerkalianMinimum(179)); // 4
-// console.log(digitPerkalianMinimum(1)); // 2
+console.log(digitPerkalianMinimum(1)); // 2
