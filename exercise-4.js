bd25bd8b036358fb69fb7588b26957f0c94edbad
andikajayaw <andikajayawiguna@gmail.com>
2019-09-11 18:40:58 +0700

    update exercise-4.js

--- exercise-4.js
+++ exercise-4.js
@@ -16,7 +16,7 @@ function cariModus(arr) {
         var isFind = false
         for (var j = 0; j < list.length; j++) {
             if (arr[i] === list[j][0]) {
-                isFind = true
+                isFind === true
                 list[j][1]++
             }
         }
@@ -25,13 +25,23 @@ function cariModus(arr) {
         }
     }
 
+    var max = list[0]
+    for (var i = 0; i < list.length; i++) {
+        if (max[1] < list[i][1]) {
+            max = list[i]
+        }
+    }
 
-    return list
+    if (max[1] === 1 || max[1] === arr.length) {
+        return -1
+    }
+
+    return max[0]
 }
 
 // TEST CASES
 console.log(cariModus([10, 4, 5, 2, 4])); // 4
-// console.log(cariModus([5, 10, 10, 6, 5])); // 5
-// console.log(cariModus([10, 3, 1, 2, 5])); // -1
-// console.log(cariModus([1, 2, 3, 3, 4, 5])); // 3
-// console.log(cariModus([7, 7, 7, 7, 7])); // -1
+console.log(cariModus([5, 10, 10, 6, 5])); // 5
+console.log(cariModus([10, 3, 1, 2, 5])); // -1
+console.log(cariModus([1, 2, 3, 3, 4, 5])); // 3
+console.log(cariModus([7, 7, 7, 7, 7])); // -1
